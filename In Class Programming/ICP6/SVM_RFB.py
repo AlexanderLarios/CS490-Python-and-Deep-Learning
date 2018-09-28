ea6fe544d6dd7e82f5fa1a6141bd7bff8fcef599
Alexander Larios <alarios91@gmail.com>
2018-09-28 18:33:48 -0500

    ICP6

---
+++ In Class Programming/ICP6/SVM_RFB.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets, metrics
+from sklearn.model_selection import train_test_split
+
+# Loading the dataset
+irisdataset = datasets.load_iris()
+
+# getting the data and response of the dataset
+x = irisdataset.data
+y = irisdataset.target
+print(irisdataset.data)
+print(irisdataset.target)
+
+from sklearn.svm import SVC
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=0)
+
+svm = SVC(kernel='rbf', random_state=0, gamma=.01, C=1)
+svm.fit(X_train, y_train)
+print('Accuracy of SVM classifier on training set: {:.2f}'.format(svm.score(X_train, y_train)))
+print('Accuracy of SVM classifier on test set: {:.2f}'.format(svm.score(X_test, y_test)))
+# Create a SVC classifier using an RBF kernel
